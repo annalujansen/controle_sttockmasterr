9e0222fe88fcb3f26d211e97d98f9164ee45a72f
annalujansen <annalu.nj@gmail.com>
2024-07-26 18:43:28 -0300

    adição de um botão de logout

--- src/app/estoque/page.tsx
+++ src/app/estoque/page.tsx
@@ -1,7 +1,21 @@
-import TabelaEstoque from "../_components/tabelaEstoque";
+"use client"
+import { useRouter } from 'next/navigation';
+import TabelaEstoque from '../_components/tabelaEstoque';
+import styles from '~/styles/estoque.module.css'
 
-export default function VisualizacaoEstoque() {
-    return (
-        <TabelaEstoque/>
-    )
+export default function Home() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    router.push('/login');
+  };
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <button className={styles.buttonHeader} onClick={handleLogout}>Logout</button>
+      </header>
+      <TabelaEstoque />
+    </div>
+  );
 }
